65f448c58a8e6997ac1370c841dc98a8678587cf
Flaimer2 <63396386+Flaimer2@users.noreply.github.com>
2024-05-31 17:55:02 +0300

    [1.1] lowercase server_mode

--- platform/platform-bukkit/src/main/kotlin/handlers/lobby/ConnectCommand.kt
+++ platform/platform-bukkit/src/main/kotlin/handlers/lobby/ConnectCommand.kt
@@ -24,7 +24,7 @@ class ConnectCommand : BaseCommand() {
     fun onServer(player: Player, args: String) {
         val server = Balancer.getServer(args)
         if (server == null) {
-            player.message(config.connectCommand().serverNotFound(), Pair("server_name", args.uppercase()))
+            player.message(config.connectCommand().serverNotFound(), Pair("server_name", args.lowercase()))
             return
         }
         if (!server.canJoin(player)) {
@@ -38,12 +38,12 @@ class ConnectCommand : BaseCommand() {
     fun onType(player: Player, args: String) {
         val type = ServerType(args)
         if (type == ServerType.UNKNOWN) {
-            player.message(config.connectCommand().unknownServerMode(), Pair("server_mode", args.capitalize()))
+            player.message(config.connectCommand().unknownServerMode(), Pair("server_mode", args.lowercase()))
             return
         }
         val server = Balancer.getBestServer(player, type).firstOrNull()
         if (server == null) {
-            player.message(config.connectCommand().modeServerNotFound(), Pair("server_mode", type.name.capitalize()))
+            player.message(config.connectCommand().modeServerNotFound(), Pair("server_mode", type.name.lowercase()))
             return
         }
         player.connect(server)
@@ -57,12 +57,12 @@ class ConnectCommand : BaseCommand() {
         }
         val type = ServerType(args[0])
         if (type == ServerType.UNKNOWN) {
-            player.message(config.connectCommand().unknownServerMode(), Pair("server_mode", args[0].capitalize()))
+            player.message(config.connectCommand().unknownServerMode(), Pair("server_mode", args[0].lowercase()))
             return
         }
         val server = Balancer.getBestServer(player, type).firstOrNull { it.mode.name.lowercase() == args[1].lowercase() }
         if (server == null) {
-            player.message(config.connectCommand().typeModeServerNotFound(), Pair("server_mode", type.name.capitalize()), Pair("server_type", args[1].lowercase()))
+            player.message(config.connectCommand().typeModeServerNotFound(), Pair("server_mode", type.name.lowercase()), Pair("server_type", args[1].lowercase()))
             return
         }
         player.connect(server)
