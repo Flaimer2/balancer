010b883727bc7957b6abeb07143a8f37ab2cb104
Flaimer2 <63396386+Flaimer2@users.noreply.github.com>
2024-07-19 21:41:03 +0300

    Update PlayerExtension.kt

--- platform/platform-velocity/src/main/kotlin/extensions/PlayerExtension.kt
+++ platform/platform-velocity/src/main/kotlin/extensions/PlayerExtension.kt
@@ -1,20 +1,21 @@
 package ru.snapix.balancer.extensions
 
 import com.velocitypowered.api.proxy.Player
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import ru.snapix.balancer.BalancerServer
-import ru.snapix.balancer.PlayerConnect
-import ru.snapix.balancer.redisKeyConnect
+import ru.snapix.balancer.*
 import ru.snapix.library.ServerType
-import ru.snapix.library.redis.async
-import ru.snapix.library.redis.redisClient
+import kotlin.jvm.optionals.getOrNull
 
 fun Player.connect(server: BalancerServer) {
     if (server.serverType == ServerType.UNKNOWN) return
-    redisClient.async {
-        publish(
-            redisKeyConnect, Json.encodeToString(PlayerConnect(username, server))
-        )
-    }
+    val registeredServer = balancerVelocity.server.getServer(server.name).getOrNull() ?: return
+
+    createConnectionRequest(registeredServer)
 }
+
+val Player.currentBalancerServer: BalancerServer?
+    get() {
+        val server = currentServer.getOrNull() ?: return null
+        val servername = server.server.serverInfo.name
+
+        return Balancer.server(servername)
+    }
